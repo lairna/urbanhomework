2715b481ae121231f2fb444a4e3cc0dd15569f91
lairna <lairna@mail.ru>
2024-09-05 01:46:15 +0300

    dfdd

--- module_4_1.py
+++ module_4_1.py
@@ -1,16 +1,19 @@
 from math import inf
 
+
 def fake_divide(first, second):
-  if second == 0:
-    return "Ошибка"
-  else:
-    return first / second
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
+
 
 def true_divide(first, second):
-  if second == 0:
-    return inf
-  else:
-    return first / second
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
 
 result1 = fake_divide(69, 3)
 result2 = fake_divide(3, 0)
