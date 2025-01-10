3000b8570e68598f3490d315e6b5ce7587dc5ae1
lairna <lairna@mail.ru>
2025-01-10 12:14:56 +0300

    dfdd

--- module_7_3/module_7_3.py
+++ module_7_3/module_7_3.py
@@ -1,55 +1,54 @@
-import os
-import re
-
-
 class WordsFinder:
-    def __init__(self, directory="."):
-
-        self.file_names = self._find_txt_files(directory)
-
-    def _find_txt_files(self, directory):
-
-        found_files = []
-        for root, _, files in os.walk(directory):
-            for file in files:
-                if file.lower().endswith(".txt"):
-                    found_files.append(os.path.join(root, file))
-        return found_files
+    def __init__(self, *file_names):
+        self.file_names = file_names
 
     def get_all_words(self):
         all_words = {}
-        punctuation = r"[,.!=?;:-]"
         for file_name in self.file_names:
-            try:
-                with open(file_name, 'r', encoding='utf-8') as f:
-                    all_text = f.read().lower()
-                    words = re.sub(punctuation, '', all_text).replace(" - ", " ").split()
-                    all_words[file_name] = words
-            except FileNotFoundError:
-                continue
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = file.read().lower()
+                for i in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    words = words.replace(i, '')
+                all_words[file_name] = words.split()
         return all_words
 
     def find(self, word):
-
         word = word.lower()
-        results = {}
-        for name, words in self.get_all_words().items():
-            try:
-                results[name] = words.index(word) + 1
-            except ValueError:
-                results[name] = None
-        return results
+        all_words = self.get_all_words()
+        list_words = {}
+        for file_name, words in all_words.items():
+            if word in words:
+                list_words[file_name] = words.index(word) + 1
+        return list_words
 
     def count(self, word):
-
         word = word.lower()
-        results = {}
-        for name, words in self.get_all_words().items():
-            results[name] = words.count(word)
-        return results
+        all_words = self.get_all_words()
+        word_counts = {}
+        for file_name, words in all_words.items():
+            word_counts[file_name] = words.count(word)
+        return word_counts
 
 
-finder3 = WordsFinder(".")
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
+print()
+
+finder3 = WordsFinder('Mother Goose - Monday’s Child.txt',)
 print(finder3.get_all_words())
-print(finder3.find("TEXT"))
-print(finder3.count("teXT"))
+print(finder3.find('Child'))
+print(finder3.count('Child'))
+print()
+
+finder4 = WordsFinder('Rudyard Kipling - If.txt',)
+print(finder4.get_all_words())
+print(finder4.find('if'))
+print(finder4.count('if'))
+print()
+
+finder5 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt')
+print(finder5.get_all_words())
+print(finder5.find('captain'))
+print(finder5.count('captain'))
